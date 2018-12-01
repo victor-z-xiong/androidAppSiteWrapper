30d160fc40770a79045e5a0781c637514b1ed6b3
lungofd <victorzxiong98@gmail.com>
2018-11-30 22:50:43 -0800

    site wrapper template

---
+++ app/src/main/java/com/example/placeholder/androidappsitewrapper/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.placeholder.androidappsitewrapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webWrapper);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://secure.celpiptest.ca/RegWebApp/#/account/sign-in");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        } else{
+            super.onBackPressed();
+        }
+    }
+}
